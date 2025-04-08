7d5de14f46848b3fd57db8c5a6c4515b62d3508e
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2025-04-08 14:19:00 +0200

    Updating version

--- wgSwiperNext.user.js
+++ wgSwiperNext.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Waifugame swiper next
 // @namespace    http://tampermonkey.net/
-// @version      2025-04-04
+// @version      2025-04-08
 // @description  Move your cards to boxes from the swiper page.
 // @author       Lulu5239
 // @match        https://waifugame.com/*

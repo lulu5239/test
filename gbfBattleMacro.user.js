cbcb7b5d5bd4a487d09a9e8721bac8e05e422751
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2025-04-17 16:02:42 +0200

    Macros list

--- gbfBattleMacro.user.js
+++ gbfBattleMacro.user.js
@@ -1,8 +1,10 @@
 // ==UserScript==
 // @name         Battle macros
-// @version      0.1
+// @version      2025-04-17
 // @description  Use skills in a specific order by pressing less buttons.
-// @author       Lalabels
+// @author       Lulu5239
+// @updateURL    https://github.com/lulu5239/test/raw/refs/heads/master/gbfBattleMacro.user.js
+// @downloadURL  https://github.com/lulu5239/test/raw/refs/heads/master/gbfBattleMacro.user.js
 // @match        *://game.granbluefantasy.jp/*
 // @match        *://gbf.game.mbga.jp/*
 // @grant        GM_getValue
@@ -12,8 +14,24 @@
 var click = e=>e.dispatchEvent(new Event("tap",{bubbles:true, cancelable:true}))
 
 var onPage = async ()=>{
-  if(!document.location.hash?.startsWith("#battle")){return}
-  
+  if(!document.location.hash?.startsWith("#battle") && !document.location.hash?.startsWith("#raid")){return}
+  document.querySelector(".ctn-raid").style.paddingBottom = null
+  let macros = GM_getValue("macros") || []
+  document.querySelector(".contents").insertAdjacentHTML("beforeend",
+    `<div id="macros-list">${macros.map(macro=>(
+      `<div class="listed-macro" data-id="${macro.id}">${macro.name}</div>`
+    ))}<div class="listed-macro" data-id="new">New...</div></div>
+    <style>
+      .listed-macro {
+        display:block;
+        width:100%;
+        padding:5px;
+        background:#222;
+        color:#fff;
+        margin-bottom:2px;
+      }
+    </style>`
+  )
 }
 
 window.addEventListener("hashchange", onPage)

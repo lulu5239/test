a86080b69d95b580be8a2b0d6450840c688801e0
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2026-05-25 14:29:26 +0200

    Prevent wasting food

--- wgSwiperNext.user.js
+++ wgSwiperNext.user.js
@@ -161,7 +161,7 @@
       if(!delayedClicks.length){hpBar.style.backgroundColor = "#da4453"}
 
       if(selectedAnimu?.id == selectedAnniemay && selectedAnimu.hpText.split(" ", 1)[0].split("/").reduce((p, n)=>(!p ? n : n===p), null) && selectedAnimu.xpText==="Max Level!" && !settings.allowWastingItems){
-        showErrorToast("The Animu doesn't need items!")
+        return showErrorToast("The Animu doesn't need items!")
       }
       let r = await fetch("/am/" + am, {
         method: "POST", 

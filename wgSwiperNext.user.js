c7b8ca42750f00962c3b5dcde4ef1b0aa907580d
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2026-07-12 20:10:43 +0200

    Remove $0

--- wgSwiperNext.user.js
+++ wgSwiperNext.user.js
@@ -941,7 +941,7 @@
         selectedCard = selectedCard[0]
       }
       if(settings.cardsListFixedSize){
-        $0.scrollTop = selectedCard.offsetTop -40
+        document.querySelector("#cardListing").scrollTop = selectedCard.offsetTop -40
       }
       let action = cards[$(selectedCard).data("card").id]
       document.querySelector(`#swiperNextButtons div[data-nextaction="${action!==undefined ? ""+action : "nothing"}"]`).click()

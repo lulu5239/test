53f326c9560df6137afa974bbaad5e10c1b0f590
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2026-08-06 20:53:28 +0200

    Fix Myfu variable

--- wgChecklist.user.js
+++ wgChecklist.user.js
@@ -63,7 +63,7 @@
       let CR = +startForm.querySelector("#myfuMissionChallengeRating").value
       trying = {
         type: "mission."+Myfus[0].dataset.id,
-        MyfuName: Myfu.querySelector("h5").innerText,
+        MyfuName: Myfus.querySelector("h5").innerText,
         t: +new Date() + Math.max(CR - Myfus.reduce((p, m)=>p+m.dataset.sp, 0), CR*0.2),
       }
     }

4bc759e9ebae0e9deed342939240b0f12b338d7b
Lulu5239 <32999164+lulu5239@users.noreply.github.com>
2026-08-09 15:33:00 +0200

    Continue to not update subscription if finished

--- wgChecklist.user.js
+++ wgChecklist.user.js
@@ -342,6 +342,7 @@
     let e = document.querySelector(".page-content center span")
     daily.createdCards = finished ? +finished.innerText.match(/\((.*?)\)/g)[0].slice(1, -1) : e ? +e.innerText : 0
     GM_setValue("daily", daily)
+    if(finished){return}
 
     e = +document.querySelector(".page-content center span:nth-child(2)")?.innerText || 0
     subscription.current = [0, 1, 5, 10].findIndex(n=>n===e)
